18ba92cb5f9392b510717a242d5b197ab89ee037
Bruno Wagler Cruz Padilha Ferreira <reidx0@Outlook.com>
2025-07-04 18:28:21 -0300

    criado os botões

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,22 @@
+<div class="flex space-x-4">
+    <a href="{{ $Inicio }}" 
+       class="bg-slate-600 text-white px-4 py-2 rounded-lg hover:bg-stone-700 transition">
+       {{ $labelInicio }}
+    </a>
+    <a href="{{ $Registro }}" 
+       class="bg-slate-600 text-white px-4 py-2 rounded-lg hover:bg-stone-700 transition">
+       {{ $labelRegistro }}
+    </a>
+    <a href="{{ $Login }}" 
+       class="bg-slate-600 text-white px-4 py-2 rounded-lg hover:bg-stone-700 transition">
+       {{ $labelLogin }}
+    </a>
+    <a href="{{ $Voltar }}" 
+       class="bg-slate-600 text-white px-4 py-2 rounded-lg hover:bg-stone-700 transition">
+       {{ $labelVoltar }}
+    </a>
+    <a href="{{ $vermural }}" 
+       class="bg-slate-600 text-white px-4 py-2 rounded-lg hover:bg-stone-700 transition">
+       {{ $labelvermural }}
+    </a>
+</div>
